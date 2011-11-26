d60a803d908cf62c29e024e61cd297fc2ad70627
Martin Stensgård <mastensg@ping.uio.no>
2011-11-26 08:24:51 +0100

    c-fast: simplify

--- c-fast/mandel.c
+++ c-fast/mandel.c
@@ -1,98 +1,72 @@
-#include <complex.h>
-#include <err.h>
 #include <inttypes.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#pragma pack(push)
-#pragma pack(1)
-
-struct tga_header {
-    uint8_t id_length;
-    uint8_t color_map_type;
-    uint8_t image_type;
-    uint16_t first_entry_index;
-    uint16_t color_map_length;
-    uint8_t color_map_entry_size;
-    uint16_t x_origin;
-    uint16_t y_origin;
-    uint16_t image_width;
-    uint16_t image_height;
-    uint8_t pixel_depth;
-    uint8_t image_descriptor;
-};
-
-#pragma pack(pop)
+#include <unistd.h>
 
 static const int w = 1024;
-static const int h = 768;
-static const float a_stride = 3.5 / w;
-static const float b_stride = -2. / h;
+static const int h = 1024;
 
-static void
-tga_write(FILE *file, const uint8_t *pixels, const uint16_t width, const uint16_t height) {
-    struct tga_header h;
-    memset(&h, 0, sizeof(struct tga_header));
-    h.image_type = 2;
-    h.y_origin = height;
-    h.image_width = width;
-    h.image_height = height;
-    h.pixel_depth = 24;
-    h.image_descriptor = 32;
+static const uint8_t header[] = {
+    0, // uint8_t id_length;
+    0, // uint8_t color_map_type;
+    2, // uint8_t image_type;
+    0, // uint16_t first_entry_index;
+    0, // 
+    0, // uint16_t color_map_length;
+    0, // 
+    0, // uint8_t color_map_entry_size;
+    0, // uint16_t x_origin;
+    0, // 
+    0, // uint16_t y_origin;
+    4, // 
+    0, // uint16_t image_width;
+    4, // 
+    0, // uint16_t image_height;
+    4, // 
+    24, // uint8_t pixel_depth;
+    32, // uint8_t image_descriptor;
+};
 
-    if(fwrite(&h, sizeof(struct tga_header), 1, file) == -1)
-        err(1, "write header");
-
-    if(fwrite(pixels, 3, width * height, file) == -1)
-        err(1, "write pixels");
-}
-
-static uint8_t
-mandelbrot(const float a, const float b) {
-    static const int pf = 1024;
-    int ca = a * pf;
-    int cb = b * pf;
+uint8_t
+mandelbrot(int ca, int cb) {
     int za = ca;
     int zb = cb;
 
-    for(uint8_t i = 127; i < 128; --i) {
-        if(za * za / pf + zb * zb / pf > 4 * pf)
+    int i;
+    for(i = 0; i < 128; ++i) {
+        int zaa = za * za >> 10;
+        int zbb = zb * zb >> 10;
+
+        if(zaa + zaa > 4 << 10)
             return i;
 
-        int na = za * za / pf - zb * zb / pf;
-        zb = 2 * za * zb / pf + cb;
-        za = na + ca;
+        zb = ((za * zb) >> 9) + cb;
+        za = zaa - zbb + ca;
     }
 
-    return 0;
+    return i;
 }
 
 int
 main(int argc, char *argv[]) {
-    uint8_t p[3 * w * h];
-
-    float b = 1;
+    write(1, header, sizeof(header));
 
     for(int y = 0; y < h; ++y) {
-        float a = -2.5;
+        uint8_t p[3 * w];
+        uint8_t *q = p;
 
         for(int x = 0; x < w; ++x) {
-            uint8_t v = 2 * mandelbrot(a, b);
+            int b = (2 << 10) - (y << 2);
+            int a = -(2 << 10) + (x << 2);
 
-            int i = 3 * (y * w + x);
+            int v = 256 - 2 * mandelbrot(a, b);
 
-            p[i + 0] = v;
-            p[i + 1] = v;
-            p[i + 2] = v;
-
-            a += a_stride;
+            *(q++) = v;
+            *(q++) = v;
+            *(q++) = v;
         }
 
-        b += b_stride;
+        write(1, p, sizeof(p));
     }
-
-    tga_write(stdout, p, w, h);
 
     return EXIT_SUCCESS;
 }
