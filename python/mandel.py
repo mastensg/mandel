5eba12ff5f6d31b8ce7762767c18a2a78f0d0fba
Martin Stensgård <mastensg@ping.uio.no>
2016-01-14 03:35:47 +0100

    python: autopep8

--- python/mandel.py
+++ python/mandel.py
@@ -8,25 +8,26 @@ w = 1024
 h = 768
 
 header = [
-        0, # id_length
-        0, # color_map_type
-        2, # image_type
-        0, # first_entry_index
-        0, #
-        0, # color_map_length
-        0, #
-        0, # color_map_entry_size
-        0, # x_origin
-        0, #
-        h % 256, # y_origin
-        h / 256, #
-        w % 256, # image_width
-        w / 256, #
-        h % 256, # image_height
-        h / 256, #
-        24, # pixel_depth
-        32, # image_descriptor
-        ]
+    0,  # id_length
+    0,  # color_map_type
+    2,  # image_type
+    0,  # first_entry_index
+    0,
+    0,  # color_map_length
+    0,
+    0,  # color_map_entry_size
+    0,  # x_origin
+    0,
+    h % 256,  # y_origin
+    h / 256,
+    w % 256,  # image_width
+    w / 256,
+    h % 256,  # image_height
+    h / 256,
+    24,  # pixel_depth
+    32,  # image_descriptor
+]
+
 
 def mandelbrot(c):
     z = c
